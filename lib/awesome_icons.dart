4320948e711cacb6cde7cbd7177324e438e65f03
JonathanGech <jonathangetachew2012@gmail.com>
2025-06-06 13:26:36 +0300

    reverted recent commit 2f3f3524b868f79bcc3a2df822c0055c923e8720

--- lib/awesome_icons.dart
+++ lib/awesome_icons.dart
@@ -8,4 +8,5 @@ export 'package:awesome_icons/widgets/awesome_icon_outline.dart';
 export 'package:awesome_icons/widgets/awesome_icon_bold.dart';
 export 'package:awesome_icons/widgets/awesome_icon_twotone.dart';
 export 'package:awesome_icons/widgets/awesome_helper.dart';
+export 'package:awesome_icons/widgets/icon_enums.dart';
 export 'package:awesome_icons/src/static/awesome_icons.dart';
